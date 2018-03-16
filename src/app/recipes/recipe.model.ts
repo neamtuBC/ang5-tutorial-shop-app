1b4daf44e58a7ca387e65bc96785527202b2b60e
neamtuBC <neamtu.bc@gmail.com>
2018-03-16 21:55:27 +0200

    -finishing services

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,15 @@
+import { IngredientMaterial } from '../shared/ingredientMaterial.model';
+
 export class Recipe {
   public name: string;
   public description: string;
   public imagePath: string;
+  public ingredients: IngredientMaterial[];
 
-  constructor(name: string, desc: string, imagePath:string) {
+  constructor(name: string, desc: string, imagePath:string, ingredients:IngredientMaterial[]) {
     this.name = name;
     this.description = desc;
     this.imagePath = imagePath;
+    this.ingredients = ingredients;
   }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,16 +1,47 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { IngredientMaterial } from '../shared/ingredientMaterial.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkLRq8bCNZ8Z42MW1cQA3z2PZVWEqRdK_zKtF_zTK5Knb4Y9au'),
-    new Recipe('Another recipe', 'This is the second recipe.', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUo-cpxBu9iTD0yiN0SWteXeaB5eWNM-YEFN8Csbkaf98xVBLwaQ'),
-    new Recipe('Another Test Recipe', 'This is a copy of first recipe', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkLRq8bCNZ8Z42MW1cQA3z2PZVWEqRdK_zKtF_zTK5Knb4Y9au')
+    new Recipe(
+      'A Test Recipe',
+      'This is a test',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkLRq8bCNZ8Z42MW1cQA3z2PZVWEqRdK_zKtF_zTK5Knb4Y9au',
+      [
+        new IngredientMaterial('Carne',1),
+        new IngredientMaterial('Pita',2)
+      ]),
+    new Recipe(
+      'Another recipe',
+      'This is the second recipe.',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUo-cpxBu9iTD0yiN0SWteXeaB5eWNM-YEFN8Csbkaf98xVBLwaQ',
+      [
+        new IngredientMaterial('Salata',3),
+        new IngredientMaterial('Varza',4)
+      ]),
+    new Recipe(
+      'Another Test Recipe',
+      'This is a copy of first recipe',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkLRq8bCNZ8Z42MW1cQA3z2PZVWEqRdK_zKtF_zTK5Knb4Y9au',
+      [
+        new IngredientMaterial('Rosii',3),
+        new IngredientMaterial('Castraveti',2)
+      ])
   ];
+
+  constructor(private shoppingListService: ShoppingListService){
+  }
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: IngredientMaterial[]){
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
