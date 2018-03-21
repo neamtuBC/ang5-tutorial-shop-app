9bc6c0da00a452693b6e503c913d606a2a001578
CompactStudio <compactstudio@iMac-CompactStudio.local>
2018-03-21 15:35:34 +0200

    -navigation -routing -new recipe/edit recipe component

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -41,6 +41,10 @@ export class RecipeService {
     return this.recipes.slice();
   }
 
+  getRecipe(id: number){
+    return this.recipes[id];
+  }
+
   addIngredientsToShoppingList(ingredients: IngredientMaterial[]){
     this.shoppingListService.addIngredients(ingredients);
   }
